a1f504b053d6caff9a89828eea681cfbe142391b
Ariba-memon <aribaabdulqadir786@gmail.com>
2024-05-20 19:30:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//var let const 
+//var es5  //
+//let const // es6 
+// let 
+//const 
+// let message = "Hello World"; // string 
+// let obj = {
+//     name: 'Noor',
+//     age: 16,
+//     developer: true
+// }
+// console.log(obj)
+// JSON 
+// let obj = {
+//     name: 'Noor',
+//     age: 16,
+//     developer: true
+// }
+// console.log(obj)
+// let student ={
+//     name: 'Maheen',
+//     age: 18,
+//     developer: true
+// }
+// console.log(JSON.stringify(student))
+//syntax_error
+//gym // gum
+// lett message:string = "Samiya"
+// console.log(message)
+// varrrr message:string = "Samiya"
+// console.log(message)
+// type_error
+// let message:string = "Hello World";
+// console.loger(message);
+//assignability_error
+// let message = "Hello World";
+// message = 6; 
+// console.log(message);
+// let student = "Ayesha"
+// student= 18
+//strongly typed syntax
+// let giaic_Student:string = 'Ayesha'
+// let giaic_Age:number = 18
+//type inference
+let student = "Talha";
+student = "Samiya";
+console.log(student);
+export {};
---
+++ index.ts
@@ -0,0 +1,76 @@
+
+//var let const 
+
+//var es5  //
+//let const // es6 
+
+// let 
+//const 
+// let message = "Hello World"; // string 
+
+
+// let obj = {
+//     name: 'Noor',
+//     age: 16,
+//     developer: true
+// }
+
+// console.log(obj)
+
+// JSON 
+// let obj = {
+//     name: 'Noor',
+//     age: 16,
+//     developer: true
+// }
+
+// console.log(obj)
+
+// let student ={
+//     name: 'Maheen',
+//     age: 18,
+//     developer: true
+// }
+
+// console.log(JSON.stringify(student))
+
+//syntax_error
+
+//gym // gum
+
+// lett message:string = "Samiya"
+// console.log(message)
+
+// varrrr message:string = "Samiya"
+// console.log(message)
+
+
+// type_error
+
+// let message:string = "Hello World";
+// console.loger(message);
+
+//assignability_error
+// let message = "Hello World";
+// message = 6; 
+// console.log(message);
+
+// let student = "Ayesha"
+// student= 18
+
+
+//strongly typed syntax
+// let giaic_Student:string = 'Ayesha'
+// let giaic_Age:number = 18
+
+
+//type inference
+
+let student = "Talha"
+student= "Samiya"
+// student = 17
+console.log(student)
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
